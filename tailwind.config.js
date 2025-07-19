2c1a83d805762b883f03ca71e0fa30c78e850d07
Ram <rameshwarpatil9689@gmail.com>
2025-07-19 09:54:46 +0000

    Add Tailwind CSS and Framer Motion dependencies; create Tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./node_modules/@heroui/theme/dist/components/(button|ripple|spinner).js",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+
+};
