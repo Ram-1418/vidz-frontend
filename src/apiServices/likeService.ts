bce6752f33cb54071955fc262e2f1e56c49ad11c
Ram <rameshwarpatil9689@gmail.com>
2025-10-04 09:37:25 +0000

    add likeservice

---
+++ src/apiServices/likeService.ts
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { apiBaseUrl } from "@/lib/constsants";
+import hadleError from "@/lib/hadleError";
+
+export const toggleVideoLike = async (videoId:string) => {
+  try {
+    const respone = await axios.post(`${apiBaseUrl}/v/${videoId}`, {});
+    return respone.data;
+  } catch (error) {
+    const message=hadleError(error)
+  console.error("Error adding comment:", message|| error);
+    throw new Error(message || "Failed to add comment");
+  }
+};
+
+export const toggleCommentLike = async (commentId:string) => {
+  try {
+    const respone = await axios.post(`${apiBaseUrl}/c/${commentId}`, {});
+    return respone.data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+export const toggleTweetLike = async (tweetId:string) => {
+  try {
+    const respone = await axios.post(`${apiBaseUrl}/t/${tweetId}`, {});
+    return respone.data;
+  } catch (error) {
+     const message=hadleError(error)
+  console.error("Error adding comment:", message|| error);
+    throw new Error(message || "Failed to add comment");
+  }
+};
+
+export const getLikedVideos = async (page = 1, limit = 10) => {
+  try {
+    const response = await axios.get(
+      `${apiBaseUrl}/videos?page=${page}&limit=${limit}`,
+      {}
+    );
+    return response.data;
+  } catch (error) {
+     const message=hadleError(error)
+  console.error("Error adding comment:", message|| error);
+    throw new Error(message || "Failed to add comment");
+  }
+};
