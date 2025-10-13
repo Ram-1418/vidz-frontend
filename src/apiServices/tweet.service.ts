a925a3cea8de58f6fe918545bc2a3f98f30378cc
Ram <rameshwarpatil9689@gmail.com>
2025-10-13 10:23:02 +0000

    refactor: improve tweet service functions for better error handling and response structure

--- src/apiServices/tweet.service.ts
+++ src/apiServices/tweet.service.ts
@@ -1,8 +1,63 @@
 import { apiBaseUrl } from "@/lib/constsants";
 import axios from "axios";
+import { data } from "react-router-dom";
+import { tr } from "zod/v4/locales";
 
-export const createTweet=async()=>{
-    const respone= await axios.post(
-        `${apiBaseUrl}/`
-    )
+export const createTweet=async(content:string)=>{
+    try {
+        const respone= await axios.post(
+            `${apiBaseUrl}/tweets/`,
+            {content},
+              { withCredentials: true } 
+        );
+        return respone.data
+    } catch (error:any) {
+        console.error("Error creating tweet",error.respone?data || error)
+    }
 }
+
+export const  deleteTweet=async(tweetId)=>{
+    try {
+        const respone= await axios.delete(
+            `${apiBaseUrl}/tweets/${tweetId}`,
+            
+            {withCredentials:true}
+        );
+        return respone.data
+        
+    } catch (error) {
+        console.error("Error during the deleting the tweet",error.respone?data || error)
+        
+    }
+}
+
+export const getUserTweet=async(userId)=>{
+    try {
+        const respone= await axios.get(
+            `${apiBaseUrl}/user/${userId}`,
+            {withCredentials:true}
+        )
+        return respone.data
+        
+    } catch (error) {
+        console.error(
+            "error fecting user tweets",
+            error.respone?.data || error
+        )
+        
+    }
+}
+
+export const updateTweet = async (tweetId, content) => {
+  try {
+    const response = await axios.patch(
+      `${apiBaseUrl}/${tweetId}`,
+      { content },
+      { withCredentials: true }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error updating the tweet", error.response?.data || error);
+    throw error;
+  }
+};
