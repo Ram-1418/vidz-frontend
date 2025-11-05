c8816ca1063632b6c4045a0f54904b76acc27144
Ram-1418 <rameshwarpatil9689@gmail.com>
2025-11-05 23:59:47 +0530

    feat: update layout in LoginWithUsername component with new grid design

--- src/components/loginwithusername.tsx
+++ src/components/loginwithusername.tsx
@@ -55,77 +55,91 @@ const LoginWithUsername = () => {
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <div className="w-full max-w-2xl bg-white/10 backdrop-blur-md  border border-white/20 dark:bg-neutral-900 dark:border-neutral-800 p-10 rounded-3xl shadow-2xl transform hover:scale-[1.01] transition-all duration-300">
-        {/* Title */}
-        <h2 className="text-2xl font-bold text-gray-800 dark:text-gray-200 text-center mb-6">
-          Welcome Back 👋
-        </h2>
-        <p className="text-center text-black-300 mb-10 font-bold text-2xl">
-          Login with your <span className="font-semibold text-blue-400">username</span> or <span className="font-semibold text-blue-400">email</span>
-        </p>
-        {/* Form */}
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-          <label className="text-gray-700  mb-10 font-bold ">
-            Username or Email
-          </label>
-          {/* Username */}
-          <div>
-            <input
-              {...register("value")}
-              placeholder="Enter username or email"
-              className="w-full px-4 py-2 rounded-md bg-neutral-800 border border-neutral-700 text-gray-200 
-                focus:border-pink-500 focus:ring-1 focus:ring-pink-500 outline-none"
-            />
-            {errors.value && (
-              <p className="text-red-500 text-sm mt-1">
-                {errors.value.message}
-              </p>
-            )}
-          </div>
+    // <div className="flex min-h-screen items-center justify-center bg-gray-100">
+    //   <div className="w-full max-w-2xl bg-white/10 backdrop-blur-md  border border-white/20 dark:bg-neutral-900 dark:border-neutral-800 p-10 rounded-3xl shadow-2xl transform hover:scale-[1.01] transition-all duration-300">
+    //     {/* Title */}
+    //     <h2 className="text-2xl font-bold text-gray-800 dark:text-gray-200 text-center mb-6">
+    //       Welcome Back 👋
+    //     </h2>
+    //     <p className="text-center text-black-300 mb-10 font-bold text-2xl">
+    //       Login with your <span className="font-semibold text-blue-400">username</span> or <span className="font-semibold text-blue-400">email</span>
+    //     </p>
+    //     {/* Form */}
+    //     <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+    //       <label className="text-gray-700  mb-10 font-bold ">
+    //         Username or Email
+    //       </label>
+    //       {/* Username */}
+    //       <div>
+    //         <input
+    //           {...register("value")}
+    //           placeholder="Enter username or email"
+    //           className="w-full px-4 py-2 rounded-md bg-neutral-800 border border-neutral-700 text-gray-200 
+    //             focus:border-pink-500 focus:ring-1 focus:ring-pink-500 outline-none"
+    //         />
+    //         {errors.value && (
+    //           <p className="text-red-500 text-sm mt-1">
+    //             {errors.value.message}
+    //           </p>
+    //         )}
+    //       </div>
 
-          {/* Password */}
-          <div>
-            <input
-              type="password"
-              {...register("password")}
-              placeholder="Enter password"
-              autoComplete="current-password"
-              className="w-full px-4 py-2 rounded-md bg-neutral-800 border border-neutral-700 text-gray-200 
-                focus:border-pink-500 focus:ring-1 focus:ring-pink-500 outline-none 
-                         "
-            />
-            {errors.password && (
-              <p className="text-red-500 text-sm mt-1">
-                {errors.password.message}
-              </p>
-            )}
-          </div>
+    //       {/* Password */}
+    //       <div>
+    //         <input
+    //           type="password"
+    //           {...register("password")}
+    //           placeholder="Enter password"
+    //           autoComplete="current-password"
+    //           className="w-full px-4 py-2 rounded-md bg-neutral-800 border border-neutral-700 text-gray-200 
+    //             focus:border-pink-500 focus:ring-1 focus:ring-pink-500 outline-none 
+    //                      "
+    //         />
+    //         {errors.password && (
+    //           <p className="text-red-500 text-sm mt-1">
+    //             {errors.password.message}
+    //           </p>
+    //         )}
+    //       </div>
 
-          {/* Submit Button */}
+    //       {/* Submit Button */}
 
-          <Button
-            type="submit"
-            disabled={isPending}
-            className={` w-full bg-black text-white font-semibold py-2 rounded-lg 
-                       hover:bg-black-700 transition`}
-          >
-            {isPending ? "Logging in..." : "Login"}
+    //       <Button
+    //         type="submit"
+    //         disabled={isPending}
+    //         className={` w-full bg-black text-white font-semibold py-2 rounded-lg 
+    //                    hover:bg-black-700 transition`}
+    //       >
+    //         {isPending ? "Logging in..." : "Login"}
 
 
-          </Button>
-        </form>
+    //       </Button>
+    //     </form>
 
-        <p className="">
-          Don’t have an account?{" "}
-          <span
-            onClick={() => navigate("/")}
-            className="text-blue-300 hover:underline cursor-pointer"
-          >
-            signup
-          </span>
-        </p>
-      </div>
+    //     <p className="">
+    //       Don’t have an account?{" "}
+    //       <span
+    //         onClick={() => navigate("/")}
+    //         className="text-blue-300 hover:underline cursor-pointer"
+    //       >
+    //         signup
+    //       </span>
+    //     </p>
+    //   </div>
+    // </div>
+
+    <div className="h-screen w-screen  mid-h-screen grid grid-cols-1  md:grid-cols-2 bg-neutral-950 text-white border border-red-600">
+    //right hand side  
+   <div>
+
+    <h1>Rameshwar patil mitr mandakl</h1>
+   </div>
+
+
+   //left hnd side
+   <div>
+<h2>helo for m chai aur code </h2>
+   </div>
     </div>
   );
 };
