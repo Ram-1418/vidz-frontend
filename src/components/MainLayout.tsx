db05526dcb981d74afefd19941d4d45cb7e52045
Ram-1418 <rameshwarpatil9689@gmail.com>
2025-11-15 22:49:13 +0530

    refactor: enhance MainLayout component with sidebar integration and improved layout structure

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
-
+import React, { useState } from "react";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
-  return (
-    <div className='min-h-screen bg-gray-50'>
-<Navbar toggleSidebar={()=>{}}/>
-    <main className='p-4'>
-        <Outlet/>
-    </main>
-    </div>
-  )
-}
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-export default MainLayout
+  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
+  const closeSidebar = () => setIsSidebarOpen(false);
+
+  return (
+    <div className="min-h-screen bg-gray-50 flex">
+      {/* Sidebar */}
+      <Sidebar isOpen={isSidebarOpen} closeSidebar={closeSidebar} />
+
+      {/* Main Content */}
+      <div className="flex-1">
+        <Navbar toggleSidebar={toggleSidebar} />
+
+        <main className="p-4">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
