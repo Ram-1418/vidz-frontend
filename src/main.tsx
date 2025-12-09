b13b8eaacaf9229e87a129695090cc4040ce657a
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-12-09 10:45:38 +0000

    update

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import {
 } from "react-router-dom";
 
 import RegisterForm from "./components/auth/registerForm.tsx";
-import LoginWithUsername from "./components/loginwithusername.tsx";
+import Login from "./components/auth/Login.tsx";
 import LogoutButton from "./components/LogoutButton.tsx";
 import VideoUpload from "./components/VideoUpload.tsx";
 import GetAllVideo from "./components/GetAllVideo.tsx";
@@ -18,6 +18,9 @@ import VideoPage from "./components/Videopage.tsx";
 import MainLayout from "./components/MainLayout.tsx";
 import AuthLayout from "./components/AuthLayout.tsx";
 import Tweet from "./components/TweetPage.tsx";
+import Protected from "./components/auth/ProtectedRoute.tsx";
+import { Provider } from "react-redux";
+import Providers from "./components/providers/providers.tsx";
 
 // ✅ Router definition
 const router = createBrowserRouter(
@@ -25,20 +28,28 @@ const router = createBrowserRouter(
     <>
       {/* Routes with Navbar */}
       <Route element={<MainLayout />}>
-        <Route path="/" element={<GetAllVideo />} /> {/* Default home page */}
+        <Route
+          path="/"
+          element={
+            <Protected>
+              <GetAllVideo></GetAllVideo>
+            </Protected>
+          }
+        />
+        {/* Default home page */}
         <Route path="videos" element={<GetAllVideo />} />
         <Route path="video/:id" element={<VideoPage />} />
         <Route path="fileupload" element={<VideoUpload />} />
         <Route path="logout" element={<LogoutButton />} />
-        <Route path="tweet" element={<Tweet/>}/>
+        <Route path="tweet" element={<Tweet />} />
         <Route path="refresh-session" element={<RefreshSession />} />
-        <Route path="videopage" element={<VideoPage/>}/>
+        <Route path="videopage" element={<VideoPage />} />
       </Route>
 
       {/* Routes WITHOUT Navbar */}
       <Route element={<AuthLayout />}>
-        <Route path="registration" element={<RegisterForm />} />
-        <Route path="login" element={<LoginWithUsername />} />
+        <Route path="signup" element={<RegisterForm />} />
+        <Route path="login" element={<Login />} />
       </Route>
     </>
   )
@@ -47,6 +58,6 @@ const router = createBrowserRouter(
 // ✅ Render app
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+      <RouterProvider router={router}/>
   </React.StrictMode>
 );
