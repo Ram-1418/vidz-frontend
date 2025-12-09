6710adad44ec69399265cc858b9ce6d7749e7b56
Ram <rameshwarpatil9689@gmail.com>
2025-12-09 10:25:54 +0000

    feat: implement video upload form with file, title, description, and thumbnail inputs

--- src/components/VideoUpload.tsx
+++ src/components/VideoUpload.tsx
@@ -1,11 +1,114 @@
-import React from 'react'
+import { uploadVideo } from "@/apiServices/videoService";
+import { useState } from "react";
 
-const VideoUpload = () => {
-  return (
-    <div>
-      hghghghg
-    </div>
-  )
+function isFile(file: unknown): file is File {
+  return file instanceof File;
 }
 
-export default VideoUpload
+const VideoUpload = () => {
+  const [videoFile, setvideoFile] = useState<File | null>(null);
+  const [title, settitle] = useState("");
+  const [description, setdescription] = useState("");
+  const [thumbnail, setThumbnail] = useState<File | null>(null);
+  const [loader, setloader] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!videoFile || !title) {
+      alert("plz upload the videfile and the title");
+    }
+    try {
+      setloader(true);
+      if (!isFile(videoFile) || !isFile(thumbnail)) {
+        return;
+      }
+      const result = await uploadVideo(videoFile, description, title, thumbnail);
+      console.log("uploaded", result);
+      alert("video upload sucessfuly");
+    } catch (error) {
+      console.log("error", error);
+    } finally {
+      setloader(false);
+    }
+  };
+  return (
+    <div className="flex items-center justify-center mt-12">
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-2xl mx-auto p-5 bg-white shadow-md rounded-md space-x-4 "
+      >
+        <div>
+          <label className="block font-medium" htmlFor="">
+            videofile
+          </label>
+          <input
+            type="file"
+            accept="video/*"
+            onChange={(e) => setvideoFile(e.target.files?.[0] || null)}
+            className="w-full border p-2 rounded mt-2"
+          />
+        </div>
+
+        <div>
+          <label className="block font-medium" htmlFor="">
+            title
+          </label>
+          <input
+            type="text"
+            placeholder="Enter the titke of the Video"
+            onChange={(e) => settitle(e.target.value)}
+            value={title}
+            className="w-full border p-2 rounded mt-2"
+          />
+        </div>
+        <div>
+          <label className="block font-medium" htmlFor="">
+            description
+          </label>
+          <input
+            type="text"
+            placeholder="Enter the descrption if the video"
+            onChange={(e) => setdescription(e.target.value)}
+            value={description}
+            className="w-full border p-2 rounded mt-2"
+          />
+        </div>
+        <div>
+          <label className="block font-medium" htmlFor="">
+            {" "}
+            Thumbnail
+          </label>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e) => setThumbnail(e.target.files?.[0] || null)}
+            className="w-full border p-2 rounded mt-2"
+          />
+        </div>
+
+        {loader && (
+          <div className="flex justify-center py-3">
+            <div className="w-8 h-8 border-4 border-blue-600 border-t-transparent rounded-full animate-spin"></div>
+          </div>
+        )}
+
+        <button
+          disabled={loader}
+          className={`w-full text-white py-2 rounded mt-5 ${
+            loader ? "bg-gray-400" : "bg-blue-600 hover:bg-blue-700"
+          }`}
+        >
+          {loader ? "Uploading" : "Submit"}
+        </button>
+        {/* <button className="w-full bg-blue-600 text-white py-2 rounded hover:bg-accent mt-5">
+          Submit
+        </button> */}
+        if () {
+          
+        }
+      </form>
+    </div>
+  );
+};
+
+export default VideoUpload;
