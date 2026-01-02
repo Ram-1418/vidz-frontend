ce7f404f5ddb22ad46ea24697efafd0b7e0f118f
Ram <rameshwarpatil9689@gmail.com>
2026-01-02 18:11:55 +0000

    remove space

--- src/pages/tweets.tsx
+++ src/pages/tweets.tsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const tweets = () => {
-
-
-const 
-    
+const Tweets = () => {
+  const [tweets, setTweets] = useState([
+    {
+      id: 1,
+      username: "ram",
+      content: "Learning React is fun 🚀",
+    },
+    {
+      id: 2,
+      username: "patil",
+      content: "Hooks make state management easy!",
+    },
+  ]);
 
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div style={{ padding: "20px" }}>
+      <h2>Tweets</h2>
 
-export default tweets
+      {tweets.map((tweet) => (
+        <div
+          key={tweet.id}
+          style={{
+            border: "1px solid #ddd",
+            padding: "10px",
+            marginBottom: "10px",
+            borderRadius: "8px",
+          }}
+        >
+          <h4>@{tweet.username}</h4>
+          <p>{tweet.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Tweets;
