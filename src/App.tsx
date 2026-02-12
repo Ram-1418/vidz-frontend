7492547a88946b968f719aecf54c4c40044b8e03
Ram <rameshwarpatil9689@gmail.com>
2026-02-12 11:22:26 +0000

    feat: add @tanstack/react-query for data fetching and update App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
 import AuthProvider from "./context/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <div className="">
-      <AuthProvider>
-        <Outlet />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <Outlet />
+        </AuthProvider>
+      </QueryClientProvider>
     </div>
   );
 };
